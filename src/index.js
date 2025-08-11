95912987d8a6c9585632494bf830a83ac5b516a1
shristi gupta <shristi.gupta406@gmail.com>
2025-08-11 20:09:58 +0530

    banner

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // We will handle styles here
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div className="container">
-      <img
-        src="/logo.png"
-        alt="Santulan Neurotherapy"
-        className="main-logo"
-      />
+      {/* Visiting Card Images Section */}
+      <div className="card-container">
+        <img
+          src="/visiting_card_eng.png"
+          alt="Visiting Card 1"
+          className="visiting-card"
+        />
+        <img
+          src="/visiting_card_hindi.png"
+          alt="Visiting Card 2"
+          className="visiting-card"
+        />
+      </div>
     </div>
   </React.StrictMode>
 );
