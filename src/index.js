8d875988d0182a1b0ddee0f9fe2adf5c596d69fa
shristi gupta <shristi.gupta406@gmail.com>
2025-09-25 22:30:41 +0530

    changess done

--- src/index.js
+++ src/index.js
@@ -7,18 +7,29 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div className="container">
-      {/* Visiting Card Images Section */}
-      <div className="card-container">
-        <img
-          src="/visiting_card_eng.png"
-          alt="Visiting Card 1"
-          className="visiting-card"
-        />
-        <img
-          src="/visiting_card_hindi.png"
-          alt="Visiting Card 2"
-          className="visiting-card"
-        />
+      <div className="card-layout">
+        {/* Left: Sticker */}
+        <div className="sticker-section">
+          <img
+            src="/nt_sticker.jpg"
+            alt="Sticker"
+            className="sticker"
+          />
+        </div>
+
+        {/* Right: Visiting Cards */}
+        <div className="cards-section">
+          <img
+            src="/visiting_card.jpg"
+            alt="Visiting Card English"
+            className="visiting-card"
+          />
+          <img
+            src="/visiting_card_hin_2.jpg"
+            alt="Visiting Card Hindi"
+            className="visiting-card"
+          />
+        </div>
       </div>
     </div>
   </React.StrictMode>
