a25c45c8ac1536650369e2299d42505becc33e4a
Shristi Gupta <shristi.gupta.406@gmail.com>
2026-08-19 21:08:01 +0530

    qrcode_gist_dynamic

--- src/pages/DQrcode.js
+++ src/pages/DQrcode.js
@@ -2,7 +2,27 @@ import { useEffect } from "react";
 
 function DQrcode() {
   useEffect(() => {
-    window.location.replace("https://santulanholisticsolutions.netlify.app/");
+    fetch(
+      `https://gist.githubusercontent.com/santulanneurotherapy/12eb2e48bcb2084e437bafda086a3c25/raw/diseases_description.json?nocache=${Date.now()}`
+    )
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Failed to load Gist");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        const redirectUrl = data.redirect?.url;
+
+        if (redirectUrl) {
+          window.location.replace(redirectUrl);
+        } else {
+          console.error("Redirect URL not found in Gist");
+        }
+      })
+      .catch((err) => {
+        console.error("Error loading redirect URL:", err);
+      });
   }, []);
 
   return null;
